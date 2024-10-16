d0f13de69136a15cfa4c064729403d67ff20667c
Off <client.off.ton@gmail.com>
2024-10-16 17:47:29 +0300

    -

--- backend/run.py
+++
@@ -1,8 +0,0 @@
-if __name__ == "__main__":
-    import asyncio
-    from api import run_api
-
-    async def main():
-        await asyncio.gather(run_api())
-
-    asyncio.run(main())
--- backend/utils/blockchain/__init__.py
+++
@@ -1,2 +0,0 @@
-from . import models as models
-from .blockchain import get_nft_collection, get_jetton_data, check_collection_ownership
